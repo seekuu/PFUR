327b7a74f4d30e6331764b1a530382c7b9840a15
seekuu <serkuz250@gmail.com>
2018-03-02 13:02:27 +0300

    Update qs3.py

--- qs3.py
+++ qs3.py
@@ -5,23 +5,26 @@ def qs(a,l,r):
 	#опорным берем элемент из центра списка
 	i=l
 	j=r
+	global cnt
 	while i<=j:
 		while a[i]<el:
 			i+=1
 		while a[j]>el:
 			j-=1
-		if i<=j:
+		if i<=j and a[i]!=a[j]:
 			a[i],a[j]=a[j],a[i]
+			cnt+=1
 			print(a)
 			i=i+1
 			j=j-1
 		#проходим по списку , находим эл-ты , стоящие "не на своих местах", меняем их местами
 	qs(a,l,j)
 	qs(a,i,r)
+cnt=0
 a=input().split()
 for k in range(len(a)):
 	a[k]=int(a[k])	
 	#переводим эл-ты списка в тип int
 qs(a,0,len(a)-1)
-print('Отсортированный массив :',a)
-
+print(a)
+print(cnt)
