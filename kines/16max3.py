82c7c11950e335680a455fcc0c0cc702205380f2
seekuu <serkuz250@gmail.com>
2018-12-19 20:53:25 +0300

    Update 16max3.py

--- kines/16max3.py
+++ kines/16max3.py
@@ -1,6 +1,8 @@
 a=[int(i) for i in input().split()]
 a.sort
 if a[0]*a[1]*a[-1]>a[-1]*a[-2]*a[-3]:
-    print('3 max el-ta:',a[0]*a[1]*a[-1])
+    print('3 max composition:',a[0]*a[1]*a[-1])
 else:
-    print('3 max el-ta:',a[-1]*a[-2]*a[-3])
+    print('3 max composition:',a[-1]*a[-2]*a[-3])
+'''max composition of 3 values ar array
+    example input : 1 2 3 4 5 -1 -2 -3'''
